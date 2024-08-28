6419785498816c06380cafbc4373435b10b1ff63
Montegasppα Cacilhας <montegasppa@cacilhas.info>
2024-08-28 00:45:33 -0300

    not that bright…

--- game/src/compass.rs
+++ game/src/compass.rs
@@ -2,6 +2,9 @@ use godot::engine::{ISprite2D, Sprite2D};
 use godot::prelude::*;
 
 
+const MIN_ALPHA: real = 0.0;
+const MAX_ALPHA: real = 0.6;
+
 #[derive(Debug, GodotClass)]
 #[class(init, base=Sprite2D)]
 pub(crate) struct Compass {
@@ -13,17 +16,17 @@ pub(crate) struct Compass {
 impl ISprite2D for Compass {
 
     fn ready(&mut self) {
-        self.change = 1.0;
+        self.change = 0.8;
     }
 
     fn process(&mut self, delta: f64) {
         let delta = delta as real;
         let change = self.change;
         let mut modulate = self.base().get_modulate();
-        modulate.a = (modulate.a + change * delta).clamp(0.0, 1.0);
-        if modulate.a == 0.0 {
+        modulate.a = (modulate.a + change * delta).clamp(MIN_ALPHA, MAX_ALPHA);
+        if modulate.a == MIN_ALPHA {
             self.change = change.abs();
-        } else if modulate.a == 1.0 {
+        } else if modulate.a == MAX_ALPHA {
             self.change = -change.abs();
         }
         self.base_mut().set_modulate(modulate);
